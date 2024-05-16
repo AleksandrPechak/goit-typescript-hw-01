8bfcefa9c51599c5a908e9426dbca9582300fbb2
AleksandrPechak <144905025+AleksandrPechak@users.noreply.github.com>
2024-05-16 15:32:47 +0300

    update

--- src/basic/4.ts
+++ src/basic/4.ts
@@ -2,8 +2,8 @@ function showMessage(message: string): void{
     console.log(message);   
 }
 
-function calc(num1: number, num2: number) {
-    return num1 + num2;
+function calc(num1: number, num2: number): number {
+  return num1 + num2;
 }
 
 function customError(): never {
